86e0737fc6aea1d7c6d8e0e7a3f2521e7d0644d5
cezarj59 <cezarj59@yahoo.com>
2022-10-31 22:56:57 -0300

    commit add metódos depósito

--- src/Consultas/ConsultaContasPessoaFisica.java
+++ src/Consultas/ConsultaContasPessoaFisica.java
@@ -2,21 +2,27 @@ package Consultas;
 
 import Conta.ContaPessoaFisica;
 
+import java.time.format.DateTimeFormatter;
+
 public class ConsultaContasPessoaFisica {
 
     public static void exibeCadastrosPF() {
 
+        DateTimeFormatter dataBr = DateTimeFormatter.ofPattern("dd/MM/yyyy' às 'HH:mm");       
+
         ContaPessoaFisica[] cadastro = Cadastros.CadastroContasPessoaFisica.getPessoaFisicaArray();
-        
+
         System.out.println("\n\n====Relatório de Contas PF Cadastradas====\n\n");
 
-        if (cadastro.length != 0) {            
+        if (cadastro.length != 0) {
 
             for (int i = 0; i < cadastro.length; i++) {
 
                 if (cadastro[i] != null) {
-                    System.out.println("\n------ Cadastro: " + (i + 1) + " ------");
-                    System.out.println("\nNome: " + cadastro[i].getDadosPessoais().getNome()
+
+                    System.out.println("\n------ Cadastro: " + (i + 1) + " ------"
+                            + "\nData e Hora do Cadastro: " + cadastro[i].getDataHora().format(dataBr)
+                            + "\nNome: " + cadastro[i].getDadosPessoais().getNome()
                             + "\nCPF: " + cadastro[i].getDadosPessoais().getCpf()
                             + "\nGenero: " + cadastro[i].getDadosPessoais().getGenero()
                             + "\nAgência: " + cadastro[i].getAgencia()
